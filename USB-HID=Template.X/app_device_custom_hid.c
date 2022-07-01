d4e08708a52c7aad9a0df19a7e5f97c0446c2553
GTech9971 <98648140+GTech9971@users.noreply.github.com>
2022-07-01 19:56:54 +0900

    USB-HIDのテンプレートプロジェクトを作成

---
+++ USB-HID=Template.X/app_device_custom_hid.c
@@ -0,0 +1,85 @@
+/** INCLUDES *******************************************************/
+#include "usb.h"
+#include "usb_device_hid.h"
+
+#include <string.h>
+
+#include "system.h"
+
+
+
+/** VARIABLES ******************************************************/
+/* Some processors have a limited range of RAM addresses where the USB module
+ * is able to access.  The following section is for those devices.  This section
+ * assigns the buffers that need to be used by the USB module into those
+ * specific areas.
+ */
+#if defined(FIXED_ADDRESS_MEMORY)
+    #if defined(COMPILER_MPLAB_C18)
+        #pragma udata HID_CUSTOM_OUT_DATA_BUFFER = HID_CUSTOM_OUT_DATA_BUFFER_ADDRESS
+        unsigned char ReceivedDataBuffer[64];
+        #pragma udata HID_CUSTOM_IN_DATA_BUFFER = HID_CUSTOM_IN_DATA_BUFFER_ADDRESS
+        unsigned char ToSendDataBuffer[64];
+        #pragma udata
+
+    #elif defined(__XC8)
+        unsigned char ReceivedDataBuffer[64] HID_CUSTOM_OUT_DATA_BUFFER_ADDRESS;
+        unsigned char ToSendDataBuffer[64] HID_CUSTOM_IN_DATA_BUFFER_ADDRESS;
+    #endif
+#else
+    unsigned char ReceivedDataBuffer[64];
+    unsigned char ToSendDataBuffer[64];
+#endif
+
+volatile USB_HANDLE USBOutHandle;    
+volatile USB_HANDLE USBInHandle;
+
+
+void APP_DeviceCustomHIDInitialize()
+{
+    //initialize the variable holding the handle for the last
+    // transmission
+    USBInHandle = 0;
+
+    //enable the HID endpoint
+    USBEnableEndpoint(CUSTOM_DEVICE_HID_EP, USB_IN_ENABLED|USB_OUT_ENABLED|USB_HANDSHAKE_ENABLED|USB_DISALLOW_SETUP);
+
+    //Re-arm the OUT endpoint for the next packet
+    USBOutHandle = (volatile USB_HANDLE)HIDRxPacket(CUSTOM_DEVICE_HID_EP,(uint8_t*)&ReceivedDataBuffer[0],64);
+}
+
+
+void APP_DeviceCustomHIDTasks()
+{   
+    /* If the USB device isn't configured yet, we can't really do anything
+     * else since we don't have a host to talk to.  So jump back to the
+     * top of the while loop. */
+    if( USBGetDeviceState() < CONFIGURED_STATE )
+    {
+        return;
+    }
+
+    /* If we are currently suspended, then we need to see if we need to
+     * issue a remote wakeup.  In either case, we shouldn't process any
+     * keyboard commands since we aren't currently communicating to the host
+     * thus just continue back to the start of the while loop. */
+    if( USBIsDeviceSuspended()== true )
+    {
+        return;
+    }
+    
+    //Check if we have received an OUT data packet from the host
+    if(HIDRxHandleBusy(USBOutHandle) == false)
+    {   
+        switch(ReceivedDataBuffer[0])
+        {
+            //結果送信
+            //USBInHandle = HIDTxPacket(CUSTOM_DEVICE_HID_EP, (uint8_t*)&ToSendDataBuffer[0],64);            
+        }
+        
+        
+        //Re-arm the OUT endpoint, so we can receive the next OUT data packet 
+        //that the host may try to send us.
+        USBOutHandle = HIDRxPacket(CUSTOM_DEVICE_HID_EP, (uint8_t*)&ReceivedDataBuffer[0], 64);
+    }
+}
